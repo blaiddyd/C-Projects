11edd9c80b499014696b98cb1fced5a87e5fe96d
Ekin <37198910+ekinbukulmez@users.noreply.github.com>
2018-08-24 09:50:06 +0800

    Add files via upload
    Determines whether a given year is a leap year

---
+++ leapyear.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Leap year should either be divible by 400, or divisble by 4 but not by 100.
+
+int main(int argc, char *argv[]) {
+	if(argc != 2) {
+		printf("Please only enter one year.\n");
+		return EXIT_FAILURE;
+	}
+	int inputYear = atoi(argv[1]);
+	if(inputYear % 400 == 0) {
+		printf("%d is a leap year!\n", inputYear);
+	}
+	else if(inputYear % 4 == 0 && inputYear % 100 != 0) {
+		printf("%d is a leap year!\n", inputYear);
+	}
+	else {
+		printf("%d is not a leap year :(\n", inputYear);
+	}
+	return EXIT_SUCCESS;
+
+}
