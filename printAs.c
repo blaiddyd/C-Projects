b3dc58a922f106de12c36c4a67f16d390f6994f0
Ekin <37198910+ekinbukulmez@users.noreply.github.com>
2018-08-25 14:30:40 +0800

    Add files via upload
    A program that prints different patterns based on user input.

---
+++ printAs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char input;
+	printf("This program displays different patterns made of asterisks.\n");
+	printf("Please pick the pattern you wish to print (a, b, c): \n");
+	scanf("%c", &input);
+	switch(input) {
+		case 'a' :
+			printf("**********\n");
+			break;
+		case 'b' :
+			for(int i = 1; i < 11; i++) {
+				printf("*\n");
+			}
+			break;
+		case 'c' :
+			for(int j = 1; j < 11; j++) {
+				printf("*****\n");
+			}
+			break;
+	}
+	return EXIT_SUCCESS;
+}
